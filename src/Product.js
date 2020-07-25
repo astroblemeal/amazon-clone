3bf8b1d39480f25a28744e57ca6bfb1c5ac1ab8d
Alexander <alexander@Alex.local>
2020-07-26 00:26:46 +0700

    finished login page

--- src/Product.js
+++ src/Product.js
@@ -34,7 +34,7 @@ function Product({ id, title, image, price, rating}) {
       {Array(rating)
         .fill()
         .map((_) => (
-          <p>⭐</p>
+          <span>⭐</span>
         ))}
       </div>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,1 @@
-import firebase from "firebase";
-
-
-const firebaseApp = firebase.initializeApp ({
-    apiKey: "AIzaSyBu2Ym1llSGmf36EpQ_IxEBtn_woE4LmMA",
-    authDomain: "clone-9c53b.firebaseapp.com",
-    databaseURL: "https://clone-9c53b.firebaseio.com",
-    projectId: "clone-9c53b",
-    storageBucket: "clone-9c53b.appspot.com",
-    messagingSenderId: "860752887434",
-    appId: "1:860752887434:web:c0dc4c007494b418857773",
-    measurementId: "G-NKFKBTE4GZ"
-  });
-
-
-  const auth = firebaseApp.auth();
-
-  export { auth };
 
